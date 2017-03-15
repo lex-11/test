aab1995714ad729e0743695d440e0fce49194568
lgebeyeh <lgebeyeh@LGEBEYEH-US.oradev.oraclecorp.com>
2017-03-15 10:47:31 -0400

    -/- "branch first commit"

---
+++ test/src/com/dot/test/House.java
@@ -0,0 +1,15 @@
+package com.dot.test;
+
+public class House {
+	
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	} 
+
+}
