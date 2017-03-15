502408a0562898d2e614a5c9be2c6aca50584b2a
leykun.gebeyehu <leykun.gebeyehu@gmail.com>
2017-03-15 15:27:34 -0400

    -/- commit

--- test/src/com/dot/test/MainApp.java
+++ test/src/com/dot/test/MainApp.java
@@ -6,7 +6,18 @@ public class MainApp {
 		// TODO Auto-generated method stub
 		
 		Car car = new Car();
-		car.setModel("BMW");		
+		car.setModel("BMW");	
+		Car car1 = new Car();
+		car1.setModel("BMW");
+		
+		Car car2 = new Car();
+		car2.setModel("BMW");
+		
+		Car car3 = new Car();
+		car3.setModel("BMW");
+		
+		Car car4 = new Car();
+		car4.setModel("BMW");
 		System.out.println(car.getModel());
 
 	}
