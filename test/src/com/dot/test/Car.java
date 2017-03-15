e47d063f6a65c908ac6204d37ffb1893383308a2
lgebeyeh <lgebeyeh@LGEBEYEH-US.oradev.oraclecorp.com>
2017-03-15 10:39:10 -0400

    -/- "Second commit to master branch"

---
+++ test/src/com/dot/test/Car.java
@@ -0,0 +1,22 @@
+package com.dot.test;
+
+public class Car {
+	private String model;
+	private String plateId;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getPlateId() {
+		return plateId;
+	}
+
+	public void setPlateId(String plateId) {
+		this.plateId = plateId;
+	}
+}
--- test/src/com/dot/test/MainApp.java
+++ test/src/com/dot/test/MainApp.java
@@ -1,5 +1,14 @@
 package com.dot.test;
 
 public class MainApp {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car car = new Car();
+		car.setModel("BMW");		
+		System.out.println(car.getModel());
 
+	}
+   
 }
